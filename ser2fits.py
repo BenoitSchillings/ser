36fe229af8c09e47b4e70fde6099edb9f2a89909
Benoit Schillings <bschillings@google.com>
2020-02-18 18:30:37 -0800

    write fits

--- ser2fits.py
+++ ser2fits.py
@@ -1,5 +1,8 @@
 from ser import Ser
 import cv2
+import numpy as np
+from astropy.io import fits
+
 
 if __name__ == "__main__":
     import sys
@@ -8,7 +11,10 @@ if __name__ == "__main__":
     fid = Ser(sys.argv[-1])
     
     for i in range(0, fid.count - 1):
-        img = fid.load_img(i) * 1.0
+        img = fid.load_img(i)
+        hdr = fits.header.Header()
+        fits.writeto(sys.argv[-1] + str(i) + ".fits", img, hdr, overwrite=True)
+
         v = img - np.min(img)
         v = img / np.max(img)
         
